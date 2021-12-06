0fe79099bdac8497c3d4038c7028e9760546ba81
SolaTyolo <solat.yolo@gmail.com>
2021-12-07 04:00:05 +0800

    refactor(event): add event module and refactor logger

--- packages/DI/src/Service/Resolver/ResovlerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace CuePhp\DI\Service\Resolver;
-
-interface ResolverInterface
-{
-    public function resolve(): object;
-}
--- packages/DI/src/Service/ServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace CuePhp\DI\Service;
-
-interface ServiceInterface
-{
-    public function resolveAsSingleton(): bool;
-}
